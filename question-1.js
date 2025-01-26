8e87ca2f7688a65f1e5ea32ddf82078cccc16b59
Raha Heidari <heidariraha761@gmail.com>
2025-01-26 11:48:26 -0800

    js

---
+++ question-1.js
@@ -0,0 +1,7 @@
+/*
+   what is the difference between an asynchronous function and a synchronous function?
+*/
+
+// ! Answer
+
+// The differences between asynchronous and synchronous include: Async is non-blocking, which means it will send multiple requests to a server. Sync is blocking — it will only send the server one request at a time and wait for that request to be answered by the server.
